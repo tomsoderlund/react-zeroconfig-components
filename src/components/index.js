28097ac356538b623ade305e8dbc90af3752843e
Tom Söderlund <tom@tomsoderlund.com>
2019-12-16 22:17:10 +0100

    Babel instead of Webpack

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as SelectDropdown } from './SelectDropdown'
+export { default as SelectMenu } from './SelectMenu'
+export { default as Table } from './Table'
+export { default as TabMenu } from './TabMenu'
--- src/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SelectDropdown } from './components/SelectDropdown'
-export { default as SelectMenu } from './components/SelectMenu'
-export { default as Table } from './components/Table'
-export { default as TabMenu } from './components/TabMenu'
