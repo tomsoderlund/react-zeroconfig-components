bfaf4fea8b7a0c7f2c4adb36a9f0dc068631e9b7
Tom Söderlund <tom@tomsoderlund.com>
2019-12-16 21:51:49 +0100

    index.js fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-export { default as Table } from './components/Table'
 export { default as SelectDropdown } from './components/SelectDropdown'
+export { default as SelectMenu } from './components/SelectMenu'
+export { default as Table } from './components/Table'
+export { default as TabMenu } from './components/TabMenu'
