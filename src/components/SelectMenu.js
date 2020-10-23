038ef68e572b551513050ac310c3115a8373bb3f
Tom Söderlund <tom@tomsoderlund.com>
2020-10-23 16:33:10 +0200

    Arrows em size

--- src/components/SelectMenu.js
+++ src/components/SelectMenu.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const Arrows = () => (
-  <svg className='arrows' width='10px' height='18px' viewBox='0 0 10 18' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
+  <svg className='arrows' width='0.71em' height='1.29em' viewBox='0 0 10 18' version='1.1' xmlns='http://www.w3.org/2000/svg' xmlnsXlink='http://www.w3.org/1999/xlink'>
     <g id='arrows' fill='gray'>
       <polygon id='up' points='5 0 10 7.5 0 7.5' />
       <polygon id='down' transform='translate(5, 13.75) rotate(-180) translate(-5, -13.75) ' points='5 10 10 17.5 0 17.5' />
